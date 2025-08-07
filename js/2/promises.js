82303a30fd0289d1d9bc629b6f34e180d36e7686
rezi <rezi.sirbiladze2@gmail.com>
2025-08-07 11:01:16 +0200

    fix promises

--- js/2/promises.js
+++ js/2/promises.js
@@ -8,4 +8,38 @@ function helloWorld() {
   });
 }
 
+function addEmoji(message) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message + ' 👋');
+    }, 1000);
+  });
+}
+
+helloWorld()
+  .then(message => {
+    console.log(message);
+    return addEmoji(message);
+  })
+  .then(emojiMessage => console.log(emojiMessage))
+  .catch(error => console.error(error))
+  .finally(() => console.log('Promise completed'));
+
+console.log('---');
+
+function getMotorcycle(id) {
+  return new Promise((resolve, reject) => {
+    resolve({ id, brand: 'Honda', model: 'CB500 Hornet', year: 2024, price: 7000 });
+  });
+}
+
+function addTax(price) {
+  return Promise.resolve(price * 1.2);
+}
+
+getMotorcycle(1)
+  .then(motorcycle => addTax(motorcycle.price))
+  .then(taxedMotorcycle => console.log(taxedMotorcycle))
+  .catch(error => console.error(error));
+
 console.log('---');
